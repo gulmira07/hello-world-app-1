4187926f285ec42ecc36f57eb5a757085ac39491
Kelly <66441983+ksalrin@users.noreply.github.com>
2021-03-23 23:08:25 -0500

    Update main.tf

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -1,4 +1,4 @@
-module "helm-deploy" {
+module "academy-deploy" {
   source                    = "fuchicorp/chart/helm"
   deployment_name           = "${var.deployment_name}"
   deployment_environment    = "${var.deployment_environment}"
